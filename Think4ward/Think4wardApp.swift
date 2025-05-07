d6b7a79bf6107f7888f5577b8c7105166bdfdecb
ZB <biyuhuaping@163.com>
2025-05-07 17:03:01 +0800

    application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions

--- Think4ward/Think4wardApp.swift
+++ Think4ward/Think4wardApp.swift
@@ -1,5 +1,13 @@
 import SwiftUI
 import AppLovinSDK
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct Think4wardApp: App {
