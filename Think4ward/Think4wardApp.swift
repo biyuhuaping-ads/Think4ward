e653fe3f060197ccc5183ea579aa39291c7e7632
ZB <biyuhuaping@163.com>
2025-05-07 12:17:50 +0800

    初始化SDK

--- Think4ward/Think4wardApp.swift
+++ Think4ward/Think4wardApp.swift
@@ -3,13 +3,15 @@ import AppLovinSDK
 
 @main
 struct Think4wardApp: App {
-    let initConfig = ALSdkInitializationConfiguration(sdkKey: "LNR7IqQoTN_ruXr55ZWa_0SoWtyL65IFWSneUVVlGsv6RXs6idmUqtaf7AilM7UX_9NOyitGTFk_0prZ75JyhZ") { builder in
-        builder.mediationProvider = ALMediationProviderMAX
-    }
-    
-    // Initialize the SDK with the configuration
-    ALSdk.shared().initialize(with: initConfig) { sdkConfig in
-        // Start loading ads
+    init() {
+        let initConfig = ALSdkInitializationConfiguration(sdkKey: "LNR7IqQoTN_ruXr55ZWa_0SoWtyL65IFWSneUVVlGsv6RXs6idmUqtaf7AilM7UX_9NOyitGTFk_0prZ75JyhZ") { builder in
+            builder.mediationProvider = ALMediationProviderMAX
+        }
+
+        ALSdk.shared().initialize(with: initConfig) { sdkConfig in
+            print("AppLovin SDK initialized.")
+            // 可以在这里预加载广告，例如 Interstitials
+        }
     }
     
     var body: some Scene {
