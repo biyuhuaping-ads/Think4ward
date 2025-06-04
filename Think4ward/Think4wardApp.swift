f011e4fcae8fec828b7e7f088409c413dc3880c7
ZB <biyuhuaping@163.com>
2025-06-04 18:34:58 +0800

    支持URL Scheme跳转

--- Think4ward/Think4wardApp.swift
+++ Think4ward/Think4wardApp.swift
@@ -12,6 +12,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct Think4wardApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State private var showAdPage = false
 
     init() {
         let initConfig = ALSdkInitializationConfiguration(sdkKey: "LNR7IqQoTN_ruXr55ZWa_0SoWtyL65IFWSneUVVlGsv6RXs6idmUqtaf7AilM7UX_9NOyitGTFk_0prZ75JyhZ") { builder in
@@ -26,7 +27,26 @@ struct Think4wardApp: App {
     
     var body: some Scene {
         WindowGroup {
-            DashboardView()
+            if showAdPage {
+                AdLaunchView()
+            } else {
+                DashboardView()
+                    .onOpenURL { url in
+                        handleIncomingURL(url)
+                    }
+            }
+        }
+    }
+    
+    // 通用的URL处理方法：Think4ward://page?name=AppLovin
+    private func handleIncomingURL(_ url: URL) {
+        if let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           let queryItems = components.queryItems,
+           queryItems.contains(where: { $0.name == "name" && $0.value == "AppLovin" }) {
+            print("接收到链接: \(url.absoluteString)")
+            print("queryItems: \(queryItems)")
+            print("Path: \(url.path)")
+            showAdPage = true
         }
     }
 }
